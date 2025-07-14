6e1e8e58c6f91202b8e5cc611552e32f7e96f458
Mohammed Waleed <mwaleedx1@gmail.com>
2025-07-14 13:37:09 +0300

    Adding WebSite Roles

---
+++ Hospital.Utilities/WebSiteRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Utilities
+{
+    public static class WebSiteRoles
+    {
+        public const string Admin = "Admin";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
+}
